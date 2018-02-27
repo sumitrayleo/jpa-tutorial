2ed94a669dd15ca2dbaa1d69e48b1f1adce9318d
Sumit Ray <sumit.ray@ehi.com>
2018-02-27 19:23:45 +0530

    Added Retrieve feature to spring data

--- jpa-10-steps/src/main/java/com/tutorial/jpa/jpa10steps/service/UserRepositoryCommandLineRunner.java
+++ jpa-10-steps/src/main/java/com/tutorial/jpa/jpa10steps/service/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.tutorial.jpa.jpa10steps.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,13 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
         User user = new User("Jill", "admin");
         userRepositoryService.save(user);
         log.info("Persisted a new user : " + user);
+        
+        Optional<User> findUser = userRepositoryService.findById(1L);
+        log.info("Retrieve user : " + findUser);
+        
+        List<User> findAllUsers = userRepositoryService.findAll();
+        log.info("Get All users : " + findAllUsers);
+        
     }
 
 }
